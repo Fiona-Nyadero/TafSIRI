0f41eac49013cc5443355fdfdc9076845784628a
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-03-04 21:27:59 +0000

    Catalogue

---
+++ templates/Steph/script.js
@@ -0,0 +1,9 @@
+// Add this function to your existing JavaScript file
+function performSearch() {
+    // Get the search input value
+    var searchQuery = document.getElementById('searchInput').value;
+
+    // Perform actions based on the search query
+    // You can send the query to your backend for processing and receive results
+     console.log('Search Query:', searchQuery);
+}
